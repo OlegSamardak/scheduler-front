70bb8c0fc735083c0f3579885bad481da35e60ab
OlegSamardak <os1998os@ukr.net>
2018-01-19 14:29:39 +0200

    created break component.

--- src/app/form/time-customization/break-selector/break-selector.component.ts
+++ src/app/form/time-customization/break-selector/break-selector.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'break-selector',
@@ -6,12 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./break-selector.component.scss']
 })
 export class BreakSelectorComponent implements OnInit {
-
-  studyBeginningTime = [
+  @Input() lesson: string;
+  breaksDuration = [
     {value: '5 хв'},
-    {value: ''},
-    {value: '09:00'},
-    {value: '09:30'}
+    {value: '10 хв'},
+    {value: '15 хв'},
+    {value: '20 хв'},
+    {value: '25 хв'},
+    {value: '30 хв'},
+    {value: '35 хв'},
+    {value: '40 хв'},
+    {value: '45 хв'},
+    {value: '50 хв'},
+    {value: '55 хв'},
+    {value: '60 хв'},
+
   ];
   constructor() { }
 
