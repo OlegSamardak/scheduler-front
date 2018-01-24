80b250aba2436ad19f445d703fb98a8210272524
Alllatell@gmail.com <KrenjuebEgW9>
2018-01-24 09:36:24 +0200

    Add lesson grid

--- src/app/schedule-input/schedule-input.module.ts
+++ src/app/schedule-input/schedule-input.module.ts
@@ -8,7 +8,7 @@ import {HttpClientModule} from "@angular/common/http";
 import {MatSelectModule} from "@angular/material/select";
 import {MatButtonModule} from "@angular/material/button";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {MatFormFieldModule, MatInputModule} from "@angular/material";
+import {MatFormFieldModule, MatInputModule, MatExpansionModule} from "@angular/material";
 import {LessonComponent} from "./day/lesson/lesson.component";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
@@ -23,7 +23,8 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatExpansionModule
   ],
   declarations: [ScheduleInputComponent, DayComponent, WeekSwitcherComponent, LessonComponent],
   exports:[
