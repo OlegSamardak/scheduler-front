b3c8b51b26342b5c3078d6bbe5635239ae40e6a2
Alllatell@gmail.com <KrenjuebEgW9>
2018-01-30 17:49:16 +0200

    Add to routing group-select and time-customization

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
-import {FormComponent} from "./form/form.component";
+import {GroupSelectComponent} from "./form/group-select/group-select.component";
+import {TimeCustomizationComponent} from "./form/time-customization/time-customization.component";
 import {ScheduleInputComponent} from "./schedule-input/schedule-input.component";
 
 const routes: Routes = [
-  { path: 'form', component: FormComponent },
-  { path: '', redirectTo: '/form', pathMatch:'full'},
+  { path: 'group-select', component: GroupSelectComponent },
+  { path: 'time-customization', component: TimeCustomizationComponent },
+  { path: '', redirectTo: '/group-select', pathMatch:'full'},
   { path: 'schedule-input', component: ScheduleInputComponent }
 ]
 
