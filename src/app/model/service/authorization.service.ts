317d32c402c5390259033731c22097be952ea203
OlegSamardak <os1998os@ukr.net>
2018-02-23 14:07:21 +0200

    changed authorization to another example. do not redirect to view, but authorize correctly.

--- src/app/model/service/authorization.service.ts
+++ src/app/model/service/authorization.service.ts
@@ -1,4 +1,4 @@
-import {Injectable} from "@angular/core";
+import {ElementRef, Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {ActivatedRoute, CanActivate, Router} from '@angular/router';
 import {Observable} from 'rxjs/Observable';
@@ -7,21 +7,45 @@ declare const gapi : any;
 
 @Injectable()
 export class AuthorizationService implements CanActivate{
+  private clientId:string = '684471065070-5cp2vm76ajefqvvql14krnsujd44resm.apps.googleusercontent.com';
+  private scope = [
+    'profile',
+    'email',
+    'https://www.googleapis.com/auth/calendar',
+  ].join(' ');
+  public auth2: any;
 
-  constructor(private http: HttpClient, private router: Router, private route: ActivatedRoute) {
-    gapi.load('auth2',function () {
-      gapi.auth2.init()
-    })
+  constructor(private http: HttpClient, private router: Router, private route: ActivatedRoute, private element: ElementRef) {
+    console.log('ElementRef: ', this.element);
+  }
+
+  public googleInit() {
+    gapi.load('auth2', () => {
+      this.auth2 = gapi.auth2.init({
+        client_id: this.clientId,
+        cookiepolicy: 'single_host_origin',
+        scope: this.scope
+      });
+      this.attachSignin(this.element.nativeElement.firstChild);
+    });
+  }
+
+  public attachSignin(element) {
+    this.auth2.attachClickHandler(element, {},
+      (googleUser) => {
+        let profile = googleUser.getBasicProfile();
+        console.log('Token || ' + googleUser.getAuthResponse().id_token);
+        console.log('ID: ' + profile.getId());
+        // localStorage.setItem('token', googleUser.getAuthResponse().id_token)
+        // ...
+      }, function (error) {
+        console.log(JSON.stringify(error, undefined, 2));
+      });
   }
 
   authorize(){
     // return this.http.get<any>(`http://localhost:9000/login/google`);
-    let googleAuth = gapi.auth2.getAuthInstance();
-    googleAuth.then(() => {
-      googleAuth.signIn({scope: 'profile email https://www.googleapis.com/auth/calendar'}).then(googleUser => {
-        console.log(googleUser.getBasicProfile());
-      });
-    });
+    this.googleInit();
   }
 
   codeForApi(code){
